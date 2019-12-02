c1d1ae51e1968b3f624f75e998feec12740b4a17
Yevhen Radchenko <yevhen.y.radchenko@gmail.com>
2019-12-02 15:55:41 +0200

    initial commit

---
+++ app/src/main/java/com/paintr/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.paintr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
